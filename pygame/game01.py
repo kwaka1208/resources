84c50b3f7b578f39d3ba13281b73a29940dd76aa
Kenichi Wakabayashi <kenichi@crssrds.jp>
2023-12-11 18:20:40 +0900

    サンプルプログラムを追加

---
+++ pygame/game01.py
@@ -0,0 +1,26 @@
+import pygame as pg
+import sys
+
+# pygame初期化
+pg.init()
+
+# 画面設定
+screen = pg.display.set_mode((800, 600)) 
+
+while True:
+    # 画面を白で塗りつぶす
+    screen.fill(pg.Color("WHITE")) 
+
+    # 赤い四角形を描画
+    pg.draw.rect(screen, pg.Color("RED"), pg.Rect(10, 20, 100, 150))
+
+    # 画面を更新
+    pg.display.update()
+
+
+    # イベントをチェック
+    for event in pg.event.get():
+        # 閉じるボタンが押されたら終了
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
