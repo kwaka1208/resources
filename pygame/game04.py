28905b9e412af1715c12e954d99c01e50b4bf605
Kenichi Wakabayashi <kenichi@crssrds.jp>
2023-12-30 22:15:39 +0900

    キャラクターの向きを変える処理を追加

---
+++ pygame/game04.py
@@ -0,0 +1,54 @@
+import pygame as pg
+import sys
+
+# pygame初期化
+pg.init()
+
+# 画面設定
+screen = pg.display.set_mode((800, 600)) 
+
+# キャラクター追加
+player = pg.image.load("images/ninja.png")
+player = pg.transform.scale(player, (100, 100))
+player = pg.transform.flip(player, True, False)
+player_rect = pg.rect.Rect(0, 480, 100, 100)
+
+# 右を向いているかどうかのフラグ
+# 右向き：True 左向き：False（右向きじゃない）
+fRight = True
+
+while True:
+    # 画面を白で塗りつぶす
+    screen.fill(pg.Color("WHITE")) 
+
+    # キー入力の情報を取得
+    key = pg.key.get_pressed()
+    if(key[pg.K_RIGHT]):
+        # 右向きキーが押された時、X座標を増やす
+        player_rect.x += 10
+        if(fRight == False):
+            # 右向きでなければ、左向きから右向きに変更
+            fRight = True
+            player = pg.transform.flip(player, True, False)
+    if(key[pg.K_LEFT]):
+        # 左向きキーが押された時、X座標を減らす
+        player_rect.x -= 10
+        if(fRight == True):
+            # 右向きなら、右向きから左向きに変更
+            fRight = False
+            player = pg.transform.flip(player, True, False)
+
+    # キャラクターを表示
+    screen.blit(player, player_rect)
+
+    # 画面を更新
+    pg.display.update()
+
+    pg.time.Clock().tick(60)
+
+    # イベントをチェック
+    for event in pg.event.get():
+        # 閉じるボタンが押されたら終了
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
