d1cacc3ec15ce4f044570132597b19e99b82197c
Kenichi Wakabayashi <kenichi@crssrds.jp>
2024-01-29 18:10:12 +0900

    ソースコード追加

---
+++ pygame/game08.py
@@ -0,0 +1,110 @@
+import pygame as pg
+import sys
+import random
+
+# pygame初期化
+pg.init()
+
+# 画面設定
+screen = pg.display.set_mode((800, 600)) 
+
+# キャラクター追加
+player = pg.image.load("images/ninja.png")
+player = pg.transform.scale(player, (100, 100))
+player = pg.transform.flip(player, True, False)
+player_rect = pg.rect.Rect(0, 480, 100, 100)
+
+# 弾追加
+bullet = pg.image.load("images/bullet.png")
+bullet = pg.transform.scale(bullet, (30, 30))
+bullet_rect = pg.rect.Rect(0, -10, 30, 30)
+
+# 敵追加
+enemy = pg.image.load("images/enemy.png")
+enemy = pg.transform.scale(enemy, (100, 100))
+enemy_rect = pg.rect.Rect(random.randint(0, 800), 0, 100, 100)
+
+# 右を向いているかどうかのフラグ
+# 右向き：True 左向き：False（右向きじゃない）
+fRight = True
+
+# 弾が敵にあたったかどうかのフラグ
+# 当たった：True 当たっない：False
+hit = False
+
+while True:
+    # 画面を白で塗りつぶす
+    screen.fill(pg.Color("GRAY")) 
+
+    # キー入力の情報を取得
+    key = pg.key.get_pressed()
+    if(key[pg.K_RIGHT]):
+        # 右向きキーが押された時、X座標を増やす
+        player_rect.x += 10
+        if(fRight == False):
+            # 右向きでなければ、左向きから右向きに変更
+            player = pg.transform.flip(player, True, False)
+            fRight = True
+    if(key[pg.K_LEFT]):
+        # 左向きキーが押された時、X座標を減らす
+        player_rect.x -= 10
+        if(fRight == True):
+            # 右向きなら、右向きから左向きに変更
+            player = pg.transform.flip(player, True, False)
+            fRight = False
+
+    # キャラクターを表示
+    screen.blit(player, player_rect)
+
+    # fire = pg.mouse.get_pressed()
+    # スペースキーが押された時に、弾が発射されていなかったら
+    if key[pg.K_SPACE] and bullet_rect.y < 0:
+        # 弾の初期位置を設定
+        bullet_rect.x = player_rect.x + 50 - 15
+        bullet_rect.y = player_rect.y
+        pg.mixer.Sound("sounds/maou_se_battle17.mp3").play()
+    if bullet_rect.y >= 0:
+        # 弾を上に移動
+        bullet_rect.y -= 10
+        if bullet_rect.y > 600:
+            
+            # 弾が画面外に出たら、初期位置に戻す
+            bullet_rect.y = -10
+        screen.blit(bullet, bullet_rect)
+
+    ## 敵の処理
+    enemy_rect.y += 10
+    if enemy_rect.y > 600:
+        if hit == True:
+            enemy = pg.transform.flip(enemy, False, True)
+            hit = False
+        enemy_rect.x = random.randint(0, 800)
+        enemy_rect.y = 0
+    screen.blit(enemy, enemy_rect)
+
+    # 敵とプレイヤーの当たり判定
+    if enemy_rect.colliderect(player_rect):
+      for i in range(10):
+        if hit != True:
+          player = pg.transform.flip(player, True, False)
+          screen.blit(enemy, enemy_rect)
+          pg.display.update()
+          pg.time.Clock().tick(60)
+      
+    # 敵と弾の当たり判定
+    if enemy_rect.colliderect(bullet_rect):
+      enemy = pg.transform.flip(enemy, False, True)
+      screen.blit(enemy, enemy_rect)
+      hit = True
+
+    # 画面を更新
+    pg.display.update()
+
+    pg.time.Clock().tick(60)
+
+    # イベントをチェック
+    for event in pg.event.get():
+        # 閉じるボタンが押されたら終了
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
